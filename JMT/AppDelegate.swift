8f33531ed4911ca538aa791ad298b1e1db68016d
hooni <jhlee909090@gmail.com>
2023-02-26 16:38:30 +0900

    webView 등록

--- JMT/AppDelegate.swift
+++ JMT/AppDelegate.swift
@@ -6,14 +6,20 @@
 //
 //
 import UIKit
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
+    }
 
-
+   // 625819013497-pim0dcaslvffl3umu725k4d3svts3oti.apps.googleusercontent.com
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
         return true
     }
 
@@ -30,7 +36,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
 
 }
 
--- JMT/ViewController.swift
+++ JMT/ViewController.swift
@@ -6,14 +6,66 @@
 //,ㅣ,ㅣ,ㅣ,
 
 import UIKit
+import GoogleSignIn
+import AuthenticationServices
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,WKUIDelegate,WKNavigationDelegate {
+    
+    @IBOutlet weak var signInView: UIView!
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func loadView() {
+        super.loadView()
+        
+        webView = WKWebView(frame: self.view.frame)
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        self.view = self.webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addButton()
+        
+        let url = URL(string: "https://www.naver.com/")
+        let request = URLRequest(url: url!)
+        //self.webView?.allowsBackForwardNavigationGestures = true  //뒤로가기 제스쳐 허용
+        webView.configuration.preferences.javaScriptEnabled = true  //자바스크립트 활성화
+        webView.load(request)
     }
+  
+    
+    func addButton() {
+        let button = ASAuthorizationAppleIDButton(authorizationButtonType: .signIn, authorizationButtonStyle: .black)
+        button.addTarget(self, action: #selector(loginHandler), for: .touchUpInside)
+        signInView.addSubview(button)
+    }
+    
+    @objc func loginHandler() {
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        request.requestedScopes = [.fullName, .email]
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.delegate = self
+        controller.presentationContextProvider = self as? ASAuthorizationControllerPresentationContextProviding
+        controller.performRequests()
+    }
+}
 
-
+extension ViewController : ASAuthorizationControllerDelegate  {
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let credential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            let user = credential.user
+            print("👨‍🍳 \(user)")
+            if let email = credential.email {
+                print("✉️ \(email)")
+            }
+        }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("error \(error)")
+    }
 }
 
