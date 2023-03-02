501190e1cfc5952f8a621de244a4d0ac03632dac
hooni <jhlee909090@gmail.com>
2023-03-02 20:14:07 +0900

    googleLogin accessToken,refreshToken 발급성공
    야호

--- JMT/API/api.swift
+++ JMT/API/api.swift
@@ -1,54 +1,59 @@
+////
+////  File.swift
+////  JMT
+////
+////  Created by 이지훈 on 2023/03/01.
+////
 //
-//  File.swift
-//  JMT
+//import Foundation
+//import UIKit
+//import GoogleSignIn
 //
-//  Created by 이지훈 on 2023/03/01.
+//enum TodoAPI {
+//    static let baseURL = "http://34.64.165.38:8080/api/v1/auth/google"
+//}
 //
-
-import Foundation
-import UIKit
-import GoogleSignIn
-
-func sendIDTokenToServer() {
-    let url = URL(string: "http://34.64.165.38:8080/api/v1/auth/google"
-)
-    // ID Token 값을 가져옴
-    guard let authentication = GIDSignIn.sharedInstance()?.currentUser?.authentication,
-          let idToken = authentication.idToken else {
-        print("ID Token not found")
-        return
-    }
-
-    // HTTP 요청 생성
-    var request = URLRequest(url: url)
-    request.httpMethod = "POST"
-
-    // HTTP 요청 헤더 설정 (Content-Type, Authorization)
-    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.setValue("Bearer \(idToken)", forHTTPHeaderField: "Authorization")
-
-    // HTTP 요청 실행
-    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-        // 에러 발생시 처리
-        if let error = error {
-            print("Error: \(error.localizedDescription)")
-            return
-        }
-
-        // 서버 응답 처리
-        guard let httpResponse = response as? HTTPURLResponse else {
-            print("Invalid response")
-            return
-        }
-        switch httpResponse.statusCode {
-        case 200..<300:
-            // 성공적인 응답 처리
-            print("ID Token sent successfully")
-        default:
-            // 에러 응답 처리
-            print("Error response: \(httpResponse.statusCode)")
-        }
-    }
-    task.resume()
-}
-}
+//func sendIDTokenToServer() {
+//    let url = URL(string: "http://34.64.165.38:8080/api/v1/auth/google"
+//)
+//    
+//    // ID Token 값을 가져옴
+//    guard let authentication = GIDSignIn.sharedInstance()?.currentUser?.authentication,
+//          let idToken = authentication.idToken else {
+//        print("ID Token not found")
+//        return
+//    }
+//
+//    // HTTP 요청 생성
+//    var request = URLRequest(url: url)
+//    request.httpMethod = "POST"
+//
+//    // HTTP 요청 헤더 설정 (Content-Type, Authorization)
+//    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+//    request.setValue("Bearer \(idToken)", forHTTPHeaderField: "Authorization")
+//
+//    // HTTP 요청 실행
+//    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+//        // 에러 발생시 처리
+//        if let error = error {
+//            print("Error: \(error.localizedDescription)")
+//            return
+//        }
+//
+//        // 서버 응답 처리
+//        guard let httpResponse = response as? HTTPURLResponse else {
+//            print("Invalid response")
+//            return
+//        }
+//        switch httpResponse.statusCode {
+//        case 200..<300:
+//            // 성공적인 응답 처리
+//            print("ID Token sent successfully")
+//        default:
+//            // 에러 응답 처리
+//            print("Error response: \(httpResponse.statusCode)")
+//        }
+//    }
+//    task.resume()
+//}
+//
--- JMT/AppDelegate.swift
+++ JMT/AppDelegate.swift
@@ -10,30 +10,70 @@ import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        return GIDSignIn.sharedInstance.handle(url)
+    
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? ) -> Bool {
+        func application(_ app: UIApplication,open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+        ) -> Bool {
+            
+            
+            GIDSignIn.sharedInstance.restorePreviousSignIn{
+                user, error in
+                if error != nil || user == nil {
+                    // Show the app's signed-out state.
+                } else {
+                    // Show the app's signed-in state.
+                }
+            }
+            return true
+            
+        }
+        return false
     }
-   
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        
-        return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
+    
 }
+//            //애플로그인
+//            let appleIDProvider = ASAuthorizationAppleIDProvider()
+//            //forUserID = userIdentifier
+//            appleIDProvider.getCredentialState(forUserID: "001281.9301aaa1f617423c9c7a64b671b6eb84.0758") { (credentialState, error) in
+//                switch credentialState {
+//                case .authorized:
+//                    // The Apple ID credential is valid.
+//                    print("해당 ID는 연동되어있습니다.")
+//                case .revoked:
+//                    // The Apple ID credential is either revoked or was not found, so show the sign-in UI.
+//                    print("해당 ID는 연동되어있지않습니다.")
+//                case .notFound:
+//                    // The Apple ID credential is either was not found, so show the sign-in UI.
+//                    print("해당 ID를 찾을 수 없습니다.")
+//                default:
+//                    break
+//                }
+//            }
+//            //앱 실행 중 강제로 연결 취소 시
+//            NotificationCenter.default.addObserver(forName: ASAuthorizationAppleIDProvider.credentialRevokedNotification, object: nil, queue: nil) { (Notification) in
+//                print("Revoked Notification")
+//                // 로그인 페이지로 이동
+//            }
+//            return true
+//        }
+//    }
+//    return true
+
+// MARK: UISceneSession Lifecycle
+
+func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    // Called when a new scene session is being created.
+    // Use this method to select a configuration to create the new scene with.
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+}
+
+func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    // Called when the user discards a scene session.
+    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+}
+
+
+
 
